08f0ecc9d38e42c269f50701baf3550664bce696
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-11-12 23:21:06 +0500

    profile information left qidmi deyarli tayyor

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,31 +1,31 @@
-import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthModalService } from './auth.modal.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+	selector: 'app-auth',
+	templateUrl: './auth.component.html',
+	styleUrl: './auth.component.scss'
 })
 export class AuthComponent implements OnInit {
-  showModal = false;
+	showModal = false;
 
-  constructor(private modalService: AuthModalService) {}
+	constructor(private modalService: AuthModalService) { }
 
-  ngOnInit() {
-    this.modalService.isModalOpen$.subscribe(isOpen => {
-      this.showModal = isOpen;
-    });
-  }
+	ngOnInit() {
+		this.modalService.isModalOpen$.subscribe(isOpen => {
+			this.showModal = isOpen;
+		});
+	}
 
-  onClose() {
-    this.modalService.closeModal();
-  }
+	onClose() {
+		this.modalService.closeModal();
+	}
 
-  @HostListener('document:click', ['$event'])
-  onOutsideClick(event: MouseEvent) {
-    const modalContent = document.querySelector('.modal-content');
-    if (this.showModal && modalContent && !modalContent.contains(event.target as Node)) {
-      this.onClose();
-    }
-  }
+	@HostListener('document:click', ['$event'])
+	onOutsideClick(event: MouseEvent) {
+		const modalContent = document.querySelector('.modal-content');
+		if (this.showModal && modalContent && !modalContent.contains(event.target as Node)) {
+			this.onClose();
+		}
+	}
 }
--- src/app/features/profile/components/information/information.component.ts
+++ src/app/features/profile/components/information/information.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-information',
-  templateUrl: './information.component.html',
-  styleUrl: './information.component.scss'
+	selector: 'app-information',
+	templateUrl: './information.component.html',
+	styleUrl: './information.component.scss'
 })
 export class InformationComponent {
+	saveUsername: boolean = false;
+	saveUsernameFN(input?: HTMLInputElement) {
+		if (!this.saveUsername && input!.value) {
+			this.showModal = true;
+		} else {
+			input!.focus()
+		}
+		if (this.saveUsername === true) {
 
+		}
+	}
+	userSuccess(input?: HTMLInputElement) {
+		if (this.showModal && input!.value) {
+			input!.disabled = true;
+			this.showModal = false;
+			this.saveUsername = true
+		}
+	}
+	sendCode() {
+
+	}
+
+
+	showModal = false;
+	onClose() {
+		this.showModal = false;
+	}
+
+	@HostListener('document:click', ['$event'])
+	onOutsideClick(event: MouseEvent) {
+		const modalContent = document.querySelector('.modal-content');
+		if (this.showModal && modalContent && !modalContent.contains(event.target as Node)) {
+			this.onClose();
+		}
+	}
 }
