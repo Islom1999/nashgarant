ede29a9348b8bfe21ce290adf098e95aafda1485
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-11-12 22:30:15 +0500

    profile qismida componentlar qo'shildi

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,22 +2,38 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { RouterModule, Routes } from '@angular/router';
+import { InformationComponent } from './components/information/information.component';
+import { BalanceComponent } from './components/balance/balance.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    component: ProfileComponent
-  }
+	{
+		path: '',
+		component: ProfileComponent,
+		children: [
+			{
+				path: 'information',
+				component: InformationComponent
+			},
+			{
+				path: 'balance', component: BalanceComponent
+			},
+			{
+				path: '**', redirectTo: 'information', pathMatch: 'full'
+			}
+		]
+	}
 ];
 
 @NgModule({
-  declarations: [
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule,
+	declarations: [
+		ProfileComponent,
+		InformationComponent,
+		BalanceComponent
+	],
+	imports: [
+		CommonModule,
 
-    RouterModule.forChild(routes)
-  ]
+		RouterModule.forChild(routes)
+	]
 })
 export class ProfileModule { }
