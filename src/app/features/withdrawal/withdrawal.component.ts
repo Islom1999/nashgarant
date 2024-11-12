6f1936d53fac1d0a3c13d88a3aa44e963b81c3bb
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-11-12 10:54:57 +0500

    change withdraw

--- src/app/features/withdrawal/withdrawal.component.ts
+++ src/app/features/withdrawal/withdrawal.component.ts
@@ -1,10 +1,18 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-withdrawal',
-  templateUrl: './withdrawal.component.html',
-  styleUrl: './withdrawal.component.scss'
+	selector: 'app-withdrawal',
+	templateUrl: './withdrawal.component.html',
+	styleUrl: './withdrawal.component.scss'
 })
 export class WithdrawalComponent {
-
+	constructor(private location: Location) { }
+	goBack(): void {
+		this.location.back();
+	}
+	activeButton: string = 'account'
+	ActiveButton(button: string) {
+		this.activeButton = button;
+	}
 }
--- src/app/features/withdrawal/withdrawal.module.ts
+++ src/app/features/withdrawal/withdrawal.module.ts
@@ -16,7 +16,6 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-
     RouterModule.forChild(routes)
   ]
 })
